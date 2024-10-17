87c1577bd837b22611da89564c74d12156acc823
giflores1026 <flores.geraldivan@gmail.com>
2024-10-17 10:24:15 +0800

    Added the login and logout

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
 
 /*
@@ -20,8 +22,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class,'login']);
+
+Route::post('/logout', [AuthController::class,'logout'])
+->middleware('auth:sanctum');
+
 Route::apiResource('courses', CourseController::class);
 
 Route::apiResource('roles', RoleController::class);
 
-Route::apiResource('users', CourseController::class);
+Route::apiResource('users', UserController::class);
