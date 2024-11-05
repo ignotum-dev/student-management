bfb99c7843df77775a38b960ebfef3237388ad5b
giflores1026 <flores.geraldivan@gmail.com>
2024-11-05 13:56:07 +0800

    Remove the gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,35 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Gate::define('index-student', function (User $current_user, User $user) {
-        //     return $current_user->id === $user->id;
-        // });
-
-        Gate::define('store-student', function (User $current_user, User $user) {
-            return $current_user->id === $user->id;
-        });
-
-        Gate::define('show-student', function (User $current_user, User $user) {
-            if ($current_user->isStudent()) {
-                return $current_user->id === $user->id;
-            }
-
-            if ($current_user->isProgramChair()) {
-                return $user->role_id === 1 || $current_user->id === $user->id;
-            }
-
-            if ($current_user->isDean()) {
-                return $user->role_id === 1 || $user->role_id === 2 || $current_user->id === $user->id;
-            }
-
-            if ($current_user->isAdmin()) {
-                return $user->role_id === 1 || $user->role_id === 2 || $user->role_id === 3 || $current_user->id === $user->id;
-            }
-
-            if ($current_user->isSuperAdmin()) {
-                return $user->role_id === 1 || $user->role_id === 2 ||$user->role_id === 3 ||$user->role_id === 4 ||
-                $user->role_id === 5;
-            }
-        });
+        //
     }
 }
