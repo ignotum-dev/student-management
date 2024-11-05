79b79d5d50bc74625dbbe61b96003eac712e21b1
giflores1026 <flores.geraldivan@gmail.com>
2024-11-05 16:40:46 +0800

    Add a user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,13 +12,66 @@ class UserPolicy
      */
     public function viewAny(User $user)
     {
-        return true;
+        if ($user->isStudent()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
      * Determine whether the user can view the model.
      */
     public function view(User $current_user, User $user)
+    {
+        // return [$current_user, $user];
+        if ($current_user->isStudent()) {
+            if ($current_user->id === $user->id) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($current_user->isProgramChair()) {
+            if ($user->role_id === 1 || $current_user->id === $user->id) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($current_user->isDean()) {
+            if ($user->role_id === 1 || $user->role_id === 2 || $current_user->id === $user->id) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($current_user->isAdmin()) {
+            if ($user->role_id === 1 || $user->role_id === 2 || $user->role_id === 3 || $current_user->id === $user->id) {
+                return true;
+            } else {
+                return false;
+            }
+        } elseif ($current_user->isSuperAdmin()) {
+            if ($user->role_id === 1 || $user->role_id === 2 || $user->role_id === 3 || $user->role_id === 4 || $current_user->id === $user->id) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user)
+    {
+        if(!$user->isStudent()) {
+            return $user->id;
+        }
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $current_user, User $user)
     {
         if ($current_user->isStudent()) {
             return $current_user->id === $user->id;
@@ -40,22 +93,6 @@ class UserPolicy
             return $user->role_id === 1 || $user->role_id === 2 ||$user->role_id === 3 ||$user->role_id === 4 ||
             $user->role_id === 5;
         }
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, User $model): bool
-    {
-        //
     }
 
     /**
